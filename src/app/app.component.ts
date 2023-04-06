043a0a6d9971da9f3f4ab7c88a1342263af5df68
Rohit Jha <jharohit25@gmail.com>
2023-04-06 17:57:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
-import { Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title:string = 'task-app';
-
-  constructor(){
-
-  }
-
-  ngOnInit():void{
-    
-  }
-
+export class AppComponent {
+  title: string = 'angular-task';
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,18 +7,19 @@ import { Component,OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title:string="Task Reminder";
 
-  constructor(){
+  title: string = 'Task-App';
+  constructor() {
 
   }
 
   ngOnInit(): void {
+  }
+
+  toggleAddTask() {
+    console.log('toggle');
     
   }
 
-  toggleaddtask(){
-    console.log("clicked in header")
-  }
 
 }
